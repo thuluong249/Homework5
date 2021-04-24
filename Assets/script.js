d56e52ef57162fc195fc91d2d02844a391491b16
Thu Luong <thuluong@Thus-MacBook-Pro.local>
2021-04-24 17:44:44 -0400

    submit the homework

---
+++ Assets/script.js
@@ -0,0 +1,72 @@
+var $currentDay = $("#currentDay");
+var $timeBlocks = $(".time-block");
+var $scheduleArea = $(".schedule");
+
+var toDoItems = [];
+//each object has a hour property and a text property
+ 
+var currentDate = moment().format("dddd, MMMM Do YYYY");
+var currentHour = moment().format("H");
+
+function setUpTimeBlocks(){
+    $timeBlocks.each(function(){
+      var $thisBlock = $(this);
+      var thisBlockHr = parseInt($thisBlock.attr("data-hour"));
+
+    });
+}
+
+function renderSchedule(){
+  
+  toDoItems = localStorage.getItem("todos");
+  toDoItems = JSON.parse(toDoItems);
+
+  //loop thru array then assign the text to the timeBlock with data-hour equal to hour. 
+  //make a variable where [data-hour={hour}] then plug it in to the selector $('[data-hour={hour}')
+  for (var i = 0; i < toDoItems.length; i++){
+    var itemHour = toDoItems[i].hour;
+    var itemText = toDoItems[i].text; 
+   
+    $("[data-hour=" + itemHour + "]").children("textarea").val(itemText);
+  }
+
+  console.log(toDoItems);
+}
+
+function saveHandler(){
+  var $thisBlock = $(this).parent();
+
+  var hourToUpdate = $(this).parent().attr("data-hour");
+  var itemToAdd = (($(this).parent()).children("textarea")).val();
+
+  //see which item we need to update based on the hour of the button clicked matching
+  for (var j = 0; j < toDoItems.length; j++){
+    if (toDoItems[j].hour == hourToUpdate){
+      //set its text to what was added to textarea
+      toDoItems[j].text = itemToAdd;
+    }
+  }
+  localStorage.setItem("todos", JSON.stringify(toDoItems));
+  renderSchedule();
+}
+
+// when the document loads
+$(document).ready(function(){
+
+  //format the timeblocks depending on time
+  setUpTimeBlocks();
+  //if there's nothing for the todos in local storage
+  if(!localStorage.getItem("todos")){
+    //initialize the array of objects
+    initializeSchedule();
+  } //otherwise dont bother bc we get it from local storage
+
+  //display current date
+  $currentDay.text(currentDate);
+
+  //render schedule from local storage
+  renderSchedule();
+  //when a todo item save button is clicked, save it
+  $scheduleArea.on("click", "button", saveHandler);
+  
+});
--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # Homework5
+For this homework I want to create a regular schedule for working day. 
+---
+* At first I need to create all the timeblocks in the HTML, added all the information for the time and textarea and a save button at the end of the row.
+---
+* Working in the JavaScript:
+
+1. Refences the heading of the page when it run, it shows the title and the current time-date-month-year for the page. 
+---
+2. And, I refences all the timeblocks that I have created in the HTML by setting up array for them.
+---
+3. For the timeblock color I create them in the HTML with the textarea by added past-present-future in front of the column, so it will change the color separately in each period of time.
+---
+4. And, into the timeblock area I can enter the event that I have to do in it and save it in the local storage. Even, when I refresh the page all the events still persist.
+---
+
+## For the main page
+![main-page](/Assets/images/main.png)
+---
+
+## Second part of the main page
+![secondpart-main-page](/Assets/images/main2.png)
+---
+
+## When refresh the page 
+![refresh-page](/Assets/images/refresh.png)
+---
